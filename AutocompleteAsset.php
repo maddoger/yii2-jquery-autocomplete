eb4a6dc491e7be10de29ce9c4c4f950f7d8405fe
Vitaliy Syrchikov <maddoger@gmail.com>
2015-11-01 02:47:20 +0400

    Fixed condition property

--- AutocompleteAsset.php
+++ AutocompleteAsset.php
@@ -9,7 +9,7 @@ class AutocompleteAsset extends AssetBundle
     public $sourcePath = '@bower/devbridge-autocomplete/dist';
 
     public $js = [
-        YII_DEBUG ? 'jquery.autocomplete.js' : 'jquery.autocomplete.min.js',
+        'jquery.autocomplete.min.js',
     ];
 
     public $depends = [
